ecdef80aa7128d965ee7d738a6a4692b543186b8
oohyun15 <sakiss4774@gmail.com>
2021-01-17 02:00:08 +0900

    Add class

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-interface Human {
+class Human {
   nickname: string
   age: number
   gender: string
+  constructor(nickname: string, age: number, gender: string) {
+    this.nickname = nickname
+    this.age = age
+    this.gender = gender
+  }
 }
 
-const person = {
-  nickname: "Potter",
-  age: 26,
-  gender: "Male"
-}
+const potter = new Human("Potter", 26, "Male")
 
 const sayHi = (person: Human) => {
   console.log(`Hi ${person.nickname}, You are ${person.age} and ${person.gender}.`)
 }
 
-sayHi(person)
+sayHi(potter)
