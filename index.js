cf10e74bdb3e19e7ea5e60b58da790cb02fbb31f
oohyun15 <sakiss4774@gmail.com>
2021-01-17 01:20:41 +0900

    Add tsc watch mode

--- index.js
+++
@@ -1,6 +0,0 @@
-const nickname = "Potter", age = "26", gender = "Male";
-const sayHi = (nickname, age, gender) => {
-    console.log(`Hi ${nickname}, You are ${age} and ${gender}.`);
-};
-sayHi(nickname, age);
-//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const sayHi = (nickname: string, age: number, gender: string) => {
+  console.log(`Hi ${nickname}, You are ${age} and ${gender}.`)
+}
+
+sayHi("Potter", 26, "Male")
